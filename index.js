237a30ce9c04a465f889f406f83e2d2379002fdc
Sa2Knight <shingo.sasaki.0529@gmail.com>
2018-01-01 02:28:21 +0900

    プロトタイプ完成

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
+const Alexa   = require('alexa-sdk');
 const transit = require('./transit.js')
-transit.fetchTransitInfo('京成曳舟駅', 'お台場駅').then((result) => console.log(result))
+
+exports.handler = function(event, context, callback) {
+  const alexa = Alexa.handler(event, context);
+  alexa.registerHandlers(handlers);
+  alexa.execute();
+};
+
+const transitMessage = (stationFrom, stationTo, transitInfo) => {
+  const msg = `
+    ${transitInfo.startTime}に${stationFrom}に到着する、
+    ${transitInfo.transport}に乗車すると、${transitInfo.arrivalTime}に
+    ${stationTo}に到着します
+  `
+  return msg.replace('行に', 'ゆきに')
+}
+
+const handlers = {
+    'LaunchRequest': function () {
+      this.emit('Transit');
+    },
+    'Transit': function () {
+      const stationFrom = this.event.request.intent.slots.StationFrom.value
+      const stationTo   = this.event.request.intent.slots.StationTo.value
+      transit.fetchTransitInfo(stationFrom, stationTo).then((result) => {
+        this.emit(':tell', transitMessage(stationFrom, stationTo, result))
+      })
+    },
+    'AMAZON.HelpIntent': function () {},
+    'AMAZON.CancelIntent': function () {},
+    'AMAZON.StopIntent': function () {}
+};
