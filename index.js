8a51348dd2af8f5f99bef79aecb72b2b66a437cd
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-12-31 20:42:24 +0900

    最小限のルート検索を行うコードを作成

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-'use strict'
-console.log('Hello,World')
+const client = require('cheerio-httpcli')
+
+client.fetch('https://transit.yahoo.co.jp/')
+  .then((result) => {
+    result.$('#sfrom').val('曳舟駅')
+    result.$('#sto').val('渋谷駅')
+    return result.$('#searchModuleSubmit').click()
+  })
+  .then((result) => {
+    const $routeDetail = result.$('.routeDetail').first()
+    const transport = $routeDetail.find('.transport').text().split('[train]')[1].trim()
+    const startTime = $routeDetail.find('.time li').first().text()
+    const arrivalTime = $routeDetail.find('.time li').last().text()
+    console.log({
+      startTime,
+      arrivalTime,
+      transport
+    })
+  })
