7d8ab93297d15cb46ddbc1d4d8df7db230883102
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-12-31 22:57:16 +0900

    路線情報の取得をモジュール化

--- index.js
+++ index.js
@@ -1,19 +1,2 @@
-const client = require('cheerio-httpcli')
-
-client.fetch('https://transit.yahoo.co.jp/')
-  .then((result) => {
-    result.$('#sfrom').val('曳舟駅')
-    result.$('#sto').val('渋谷駅')
-    return result.$('#searchModuleSubmit').click()
-  })
-  .then((result) => {
-    const $routeDetail = result.$('.routeDetail').first()
-    const transport = $routeDetail.find('.transport').text().split('[train]')[1].trim()
-    const startTime = $routeDetail.find('.time li').first().text()
-    const arrivalTime = $routeDetail.find('.time li').last().text()
-    console.log({
-      startTime,
-      arrivalTime,
-      transport
-    })
-  })
+const transit = require('./transit.js')
+transit.fetchTransitInfo('京成曳舟駅', 'お台場駅').then((result) => console.log(result))
---
+++ transit.js
@@ -0,0 +1,24 @@
+const client = require('cheerio-httpcli')
+exports.fetchTransitInfo = (stationFrom, stationTo) => {
+  return client.fetch('https://transit.yahoo.co.jp/')
+    .then((result) => {
+      result.$('#sfrom').val(stationFrom)
+      result.$('#sto').val(stationTo)
+      return result.$('#searchModuleSubmit').click()
+    })
+    .then((result) => {
+      const $routeDetail = result.$('.routeDetail').first()
+      const transport = $routeDetail.find('.transport')
+                        .text()
+                        .split(/\r\n|\r|\n/)
+                        .filter((line) => line.indexOf('[train]') >= 0)[0]
+                        .split('[train]')[1];
+      const startTime = $routeDetail.find('.time li').first().text()
+      const arrivalTime = $routeDetail.find('.time li').last().text()
+      return {
+        startTime,
+        arrivalTime,
+        transport
+      }
+    })
+}
